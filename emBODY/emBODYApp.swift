7e9312f85a441ec5f06dfd8abb931cc61c0b4f77
Nathan Dang <nd432@cornell.edu>
2024-07-25 15:10:32 -0400

    Initial Commit

---
+++ emBODY/emBODYApp.swift
@@ -0,0 +1,17 @@
+//
+//  emBODYApp.swift
+//  emBODY
+//
+//  Created by Nathan Dang on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct emBODYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
